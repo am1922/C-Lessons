20e1457eb717b42d626f99045aa2a10c9b7925c3
Artem Mikhaylov <m.av1922@yandex.ru>
2023-02-26 11:40:31 +0300

    solved task 4

--- homeworks/homework_seminar_1/Program.cs
+++ homeworks/homework_seminar_1/Program.cs
@@ -3,23 +3,40 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Write("Input first number -> ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number -> ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input first number -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 != num2)
-{
-    if(num1 > num2)
-    {
-        Console.WriteLine($"First number {num1} bigest then second number {num2}");
-    }
-    else
-    {
-        Console.WriteLine($"Second number {num2} bigest then first number {num1}");
-    }
-}
-else
-{
-    Console.WriteLine("First number and second number are equals");
-}
+// if (num1 != num2)
+// {
+//     if(num1 > num2)
+//     {
+//         Console.WriteLine($"First number {num1} bigest then second number {num2}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Second number {num2} bigest then first number {num1}");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("First number and second number are equals");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Input three numbers and the application will determine which one is the maximum");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > num1) max = num2;
+if (num3 > num2) max = num3;
+
+Console.WriteLine($"Maximum of three numbers is {max}");
