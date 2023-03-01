06045cbc0703f168e70189c63597e4f906d39ede
Artem Mikhaylov <m.av1922@yandex.ru>
2023-03-01 18:14:50 +0300

    solved task 13

--- homeworks/homework_seminar_2/Program.cs
+++ homeworks/homework_seminar_2/Program.cs
@@ -18,28 +18,47 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-// int ThirdDigit(int num)
-// {
-  
-// }
+int ThirdDigit(int num)
+{
+    while(num > 999)
+    {
+        num /= 10;
+    }
+    return num % 10;
+}
 
-// int number = Convert.ToInt32(Console.ReadLine());
+bool ValidateNum(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (ValidateNum(number))
+{
+    Console.WriteLine(ThirdDigit(number));
+}
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
 
-string Weekend(int num)
-{
-    if (num < 8 & num > 0)
-    {
-        if (num == 6 | num == 7) return "yes";
-        else return "no";
-    }
-    else return "Число не в диапазоне от 1 до 7";
-}
+// string Weekend(int num)
+// {
+//     if (num < 8 & num > 0)
+//     {
+//         if (num == 6 | num == 7) return "yes";
+//         else return "no";
+//     }
+//     else return "Число не в диапазоне от 1 до 7";
+// }
 
-int number = Convert.ToInt32(Console.ReadLine());
+// int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"{number} -> {Weekend(number)}");
+// Console.WriteLine($"{number} -> {Weekend(number)}");
