3de28d99c634fadfa8455cc593768083b49f7068
Artem Mikhaylov <m.av1922@yandex.ru>
2023-03-01 17:57:52 +0300

    solved task 15

--- homeworks/homework_seminar_2/Program.cs
+++ homeworks/homework_seminar_2/Program.cs
@@ -3,22 +3,43 @@
 // 782 -> 8
 // 918 -> 1
 
-int DecNum(int num)
-{
-    int dec = (num / 10) % 10;
-   return dec;
-}
+// int DecNum(int num)
+// {
+//     int dec = (num / 10) % 10;
+//    return dec;
+// }
 
-int number1 = Convert.ToInt32(Console.ReadLine());
+// int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(DecNum(number1));
+// Console.WriteLine(DecNum(number));
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// int ThirdDigit(int num)
+// {
+  
+// }
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+string Weekend(int num)
+{
+    if (num < 8 & num > 0)
+    {
+        if (num == 6 | num == 7) return "yes";
+        else return "no";
+    }
+    else return "Число не в диапазоне от 1 до 7";
+}
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{number} -> {Weekend(number)}");
