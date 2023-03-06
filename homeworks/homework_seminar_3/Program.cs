560938422eecbf51680be890669f82fb6a3107e7
Artem Mikhaylov <m.av1922@yandex.ru>
2023-03-06 20:31:17 +0300

    solved task 23

--- homeworks/homework_seminar_3/Program.cs
+++ homeworks/homework_seminar_3/Program.cs
@@ -28,34 +28,54 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-double FindDistace3D(double x1, double y1, double z1, double x2, double y2, double z2)
-{
-    return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
-}
+// double FindDistace3D(double x1, double y1, double z1, double x2, double y2, double z2)
+// {
+//     return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
+// }
 
-Console.WriteLine("Input coordinates X of A point: ");
-double x1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinates X of A point: ");
+// double x1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Input coordinates Y of A point: ");
-double y1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinates Y of A point: ");
+// double y1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Input coordinates Z of A point: ");
-double z1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinates Z of A point: ");
+// double z1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Input coordinates X of B point: ");
-double x2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinates X of B point: ");
+// double x2 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Input coordinates Y of B point: ");
-double y2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinates Y of B point: ");
+// double y2 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Input coordinates Z of B point: ");
-double z2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinates Z of B point: ");
+// double z2 = Convert.ToDouble(Console.ReadLine());
 
-double distance = FindDistace3D(x1, y1, z1, x2, y2, z2);
-Console.WriteLine($"Distace between A and B is {distance}");
+// double distance = FindDistace3D(x1, y1, z1, x2, y2, z2);
+// Console.WriteLine($"Distace between A and B is {distance}");
 
 
 // Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+void CubeNumber(double N)
+{
+    int i = 1;
+    while (i <= N)
+    {
+        if (i == N)
+        {
+            Console.Write($"{Math.Pow(i, 3)}");
+            break;
+        }
+        Console.Write($"{Math.Pow(i, 3)}, ");
+        i++;
+    }
+}
+
+Console.WriteLine("Input N: ");
+double N = Convert.ToDouble(Console.ReadLine());
+Console.Write($"{N} -> ");
+CubeNumber(N);
