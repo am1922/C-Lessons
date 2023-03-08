06f80ea007cd1d17b747ab56a425f8442ecbf623
Artem Mikhaylov <m.av1922@yandex.ru>
2023-03-08 12:59:08 +0300

    sloved task 29

--- homeworks/homework_seminar_4/Program.cs
+++ homeworks/homework_seminar_4/Program.cs
@@ -25,22 +25,52 @@
 // 82 -> 10
 // 9012 -> 12
 
-int FindSum (int n)
-{
-    int sum = 0;
-    while (n > 0)
-    {
-        sum += n % 10;
-        n = n / 10;
-    }
-    return sum;
-}
+// int FindSum (int n)
+// {
+//     int sum = 0;
+//     while (n > 0)
+//     {
+//         sum += n % 10;
+//         n = n / 10;
+//     }
+//     return sum;
+// }
 
-Console.WriteLine ("Input number:");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Input number:");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine ($"{n} -> {FindSum(n)}");
+// Console.WriteLine ($"{n} -> {FindSum(n)}");
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return  newArray;
+}
+
+void ShowArray (int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine ("Input array size:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Input minimum value of array element:");
+int minValue = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Input maximum value of array element:");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+ShowArray(CreateRandomArray(size, minValue, maxValue));
