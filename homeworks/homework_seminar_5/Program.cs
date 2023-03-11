ee7abc779caef9d7e2f4780ccb0fd2abd1f4655f
Artem Mikhaylov <m.av1922@yandex.ru>
2023-03-11 10:35:36 +0300

    sloved task 36

--- homeworks/homework_seminar_5/Program.cs
+++ homeworks/homework_seminar_5/Program.cs
@@ -1,11 +1,54 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int[] CreateRandomArray (int size)
-{
-    int minValue = 100;
-    int maxValue = 999;
+// int[] CreateRandomArray (int size)
+// {
+//     int minValue = 100;
+//     int maxValue = 999;
 
+//     int[] newArray = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         newArray[i] = new Random().Next(minValue, maxValue+1);
+//     }
+//     return newArray;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write (array[i] + " ");
+//     }
+//     Console.WriteLine ();
+// }
+
+// int EvenCount(int[] array)
+// {
+//     int count = 0;
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] % 2 == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// Console.WriteLine("Input array size:"); 
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = CreateRandomArray(size); 
+// ShowArray(array);
+// Console.WriteLine(EvenCount(array));
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateRandomArray (int size, int minValue, int maxValue)
+{
     int[] newArray = new int[size];
     for(int i = 0; i < size; i++)
     {
@@ -23,32 +66,29 @@ void ShowArray (int[] array)
     Console.WriteLine ();
 }
 
-int EvenCount(int[] array)
+int FindSumOddElements(int[] array)
 {
-    int count = 0;
+    int sum = 0;
     for(int i = 0; i < array.Length; i++)
     {
-        if(array[i] % 2 == 0)
+        if(array[i] % 2 != 0)
         {
-            count++;
+            sum = sum + array[i];
         }
     }
-    return count;
+    return sum;
 }
 
 Console.WriteLine("Input array size:"); 
-int size = Convert.ToInt32(Console.ReadLine());
+int size = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Input minimal value of array element:"); 
+int minValue = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Input maximal value of array element:"); 
+int maxValue = Convert.ToInt32(Console.ReadLine()); 
 
-int[] array = CreateRandomArray(size); 
-ShowArray(array);
-Console.WriteLine(EvenCount(array));
-
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
-
-
-
+int[] array = CreateRandomArray(size, minValue, maxValue); 
+ShowArray(array); 
+Console.WriteLine(FindSumOddElements(array));
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
