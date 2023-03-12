2339056041db9cbfdcf600e2e4236ba09fed583b
Artem Mikhaylov <m.av1922@yandex.ru>
2023-03-12 22:45:28 +0300

    sloved task 38

--- homeworks/homework_seminar_5/Program.cs
+++ homeworks/homework_seminar_5/Program.cs
@@ -47,48 +47,92 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-int[] CreateRandomArray (int size, int minValue, int maxValue)
+// int[] CreateRandomArray (int size, int minValue, int maxValue)
+// {
+//     int[] newArray = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         newArray[i] = new Random().Next(minValue, maxValue+1);
+//     }
+//     return newArray;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write (array[i] + " ");
+//     }
+//     Console.WriteLine ();
+// }
+
+// int FindSumOddElements(int[] array)
+// {
+//     int sum = 0;
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] % 2 != 0)
+//         {
+//             sum = sum + array[i];
+//         }
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("Input array size:"); 
+// int size = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Input minimal value of array element:"); 
+// int minValue = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Input maximal value of array element:"); 
+// int maxValue = Convert.ToInt32(Console.ReadLine()); 
+
+// int[] array = CreateRandomArray(size, minValue, maxValue); 
+// ShowArray(array); 
+// Console.WriteLine(FindSumOddElements(array));
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] CreateRandomArray (int size)
 {
-    int[] newArray = new int[size];
+    double[] newArray = new double[size];
     for(int i = 0; i < size; i++)
     {
-        newArray[i] = new Random().Next(minValue, maxValue+1);
+        newArray[i] = new Random().NextDouble() * new Random().Next(1, 100);
     }
     return newArray;
 }
 
-void ShowArray (int[] array)
+void ShowArray (double[] array)
 {
     for(int i = 0; i < array.Length; i++)
     {
-        Console.Write (array[i] + " ");
+        Console.Write (Math.Round(array[i], 2) + " ");
     }
     Console.WriteLine ();
 }
 
-int FindSumOddElements(int[] array)
+double FindMaxMinDif(double[] array)
 {
-    int sum = 0;
-    for(int i = 0; i < array.Length; i++)
+    double max = array[0];
+    double min = array[0];
+    for(int i = 1; i < array.Length; i++)
     {
-        if(array[i] % 2 != 0)
+        if(array[i] > max)
         {
-            sum = sum + array[i];
+            max = array[i];
+        }
+        else if(array[i] < min)
+        {
+            min = array[i];
         }
     }
-    return sum;
+    return max - min;
 }
 
 Console.WriteLine("Input array size:"); 
 int size = Convert.ToInt32(Console.ReadLine()); 
-Console.WriteLine("Input minimal value of array element:"); 
-int minValue = Convert.ToInt32(Console.ReadLine()); 
-Console.WriteLine("Input maximal value of array element:"); 
-int maxValue = Convert.ToInt32(Console.ReadLine()); 
 
-int[] array = CreateRandomArray(size, minValue, maxValue); 
+double[] array = CreateRandomArray(size); 
 ShowArray(array); 
-Console.WriteLine(FindSumOddElements(array));
-
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-// [3 7 22 2 78] -> 76
+Console.WriteLine(Math.Round(FindMaxMinDif(array), 2));
